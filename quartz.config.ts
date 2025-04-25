74ce94402c25320eaa73e96f3743a322f67cf47b
Jasper Goens <jasper_goens@hotmail.com>
2025-04-25 15:53:25 +0200

    update pagetitle

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Dragon's Guide",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
